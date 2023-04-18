04d1fcb3003df8d8718675342c8f02a398c973c9
Dennis Eder <dennis.eder91@web.de>
2023-04-18 21:11:18 +0200

    Example improved

--- example.py
+++ example.py
@@ -1,111 +1,139 @@
 from adaptive_cards.card import AdaptiveCard
-from adaptive_cards.elements import TextBlock, Media, MediaSource, Image
-from adaptive_cards.containers import Container, ContainerT, ColumnSet, Column, FactSet, Fact
+from adaptive_cards.elements import TextBlock, Image
+from adaptive_cards.containers import Container, ContainerT, ColumnSet, Column
 import adaptive_cards.card_types as types
 from adaptive_cards.actions import ActionToggleVisibility, TargetElement
 
 # ------------ PR examples ------------ #
 
 containers: list[ContainerT] = list()
-containers.append(Container(
-    items=[
-        TextBlock(
-            text="**Pull Requests**",
-            size=types.FontSize.EXTRA_LARGE,
-            style=types.TextBlockStyle.HEADING
-        ),
-    ],
-    style=types.ContainerStyle.EMPHASIS
-))
+containers.append(
+    Container(
+        items=[
+            TextBlock(
+                text="**Results**",
+                size=types.FontSize.EXTRA_LARGE,
+                horizontal_alignment=types.HorizontalAlignment.CENTER
+            ),
+        ],
+        style=types.ContainerStyle.EMPHASIS,
+        bleed=True
+   	)
+)
 
-containers.append(Container(
-    items=[
-        TextBlock(
-            text="**Some numbers for you**", 
-            size=types.FontSize.MEDIUM, 
-            separator=True
-        ),
-        ColumnSet(
-            columns=[
-                Column(
-                    items=[
-                        TextBlock(text="_Total_"),
-                        TextBlock(text="_Created by your team_"),
-                        TextBlock(text="_Created by other teams_"),
-                        TextBlock(text="_Reviewed_"),
-                        TextBlock(text="_Open_"),
-                        TextBlock(text="_Approved_"),
-                    ]
-                ),
-                Column(
-                    items=[
-                        TextBlock(text="5"),
-                        TextBlock(text="4"),
-                        TextBlock(text="3"),
-                        TextBlock(text="6"),
-                        TextBlock(text="1"),
-                        TextBlock(text="1"),
-                    ],
-                    spacing=types.Spacing.MEDIUM,
-                    rtl=True,
-                ),
-            ]
-        ),
-    ],
-    separator=True,
-))
-
-containers.append(Container(
-    items=[
-        TextBlock(
-            text="**Open Pull Requests**",
-            size=types.FontSize.MEDIUM,
-            style=types.TextBlockStyle.HEADING
-        ),
-    ],
-    separator=True,
-))
-
-el = TextBlock(text="Hallo", id="element")
-
-containers.append(Container(
-    items=[
-        ColumnSet(
-            columns=[
-                Column(items=[TextBlock(text="test1")]),
-                Column(items=[TextBlock(text="test1")]),
-                Column(
-                    items=[
-                        TextBlock(text="show"),
-                        TextBlock(text="Hide")
-                    ],
-                    select_action=ActionToggleVisibility(
-                        target_elements=[
-                            TargetElement(
-                                element_id="element",
-                                is_visible=False,
-                            ),
-                            TargetElement(
-                                element_id="show",
-                                is_visible=False,
-                            ),
-                            TargetElement(
-                                element_id="hide",
-                                is_visible=False,
-                            ),
-                            
-                        ],
-                        
+containers.append(
+    Container(
+        items=[
+            TextBlock(
+                text="**Some numbers for you**", 
+                size=types.FontSize.MEDIUM, 
+            ),
+            ColumnSet(
+                columns=[
+                    Column(
+                        items=[
+                            TextBlock(text="_Total_"),
+                            TextBlock(text="_Done by you_"),
+                            TextBlock(text="_Done by other teams_"),
+                            TextBlock(text="_Still open_"),
+                            TextBlock(text="_Closed_"),
+                        ]
                     ),
-                ),
-            ]
-        )
-    ]
+                    Column(
+                        items=[
+                            TextBlock(text="5"),
+                            TextBlock(text="4"),
+                            TextBlock(text="3"),
+                            TextBlock(text="6"),
+                            TextBlock(text="1"),
+                        ],
+                        spacing=types.Spacing.MEDIUM,
+                        rtl=True,
+                    )
+                ],
+                separator=True
+            ),
+        ],
+        spacing=types.Spacing.MEDIUM
+    )
+)
+
+containers.append(Container(
+    items=[
+        TextBlock(
+            text="**Detailed Results**",
+            size=types.FontSize.MEDIUM,
+        ),
+    ],
+    separator=True,
+    spacing=types.Spacing.EXTRA_LARGE
 ))
 
+containers.append(
+    Container(
+        items=[
+            Container(
+                items=[
+                    ColumnSet(
+                        columns=[
+                            Column(items=[TextBlock(text="**Number**")]),
+                            Column(items=[TextBlock(text="**Status**")]),
+                            Column(items=[TextBlock(text="**Topic**")]),
+                            Column(items=[TextBlock(text="")]),
+                        ],
+                        id="headline"
+                    ),       
+                ],
+                style=types.ContainerStyle.EMPHASIS,
+                bleed=True,
+            ),
+            Container(
+                items=[
+                    ColumnSet(
+                        columns=[
+                            Column(items=[TextBlock(text="12312")]),
+                            Column(items=[TextBlock(text="done", color=types.Colors.GOOD)]),
+                            Column(items=[TextBlock(text="abc")]),
+                            Column(
+                                items=[
+                                    Image(
+                                        url="https://adaptivecards.io/content/down.png", 
+                                        width="20px",
+                                        horizontal_alignment=types.HorizontalAlignment.RIGHT
+                                    )
+                                ],
+                                select_action=ActionToggleVisibility(
+                                    title="More",
+                                    target_elements=[
+                                        TargetElement(
+                                            element_id="toggle-me",
+                                        )
+                                    ],
+                                    
+                                )
+                            ),
+                        ]
+                    ),       
+                ]
+            ),
+            Container(
+                items=[
+                    TextBlock(
+                        text="_Here you gonna find more information about the whole topic_", 
+                        id="toggle-me", 
+                        is_visible=False, 
+                        is_subtle=True,
+                        wrap=True,
+                    )
+                ]
+            )
+        ],
+    )
+)
 
-card = AdaptiveCard.new().add_items(containers).create()
+
+card = AdaptiveCard.new().version("1.4").add_items(containers).create()
   
-with open("text.json", "w+") as f:
+with open("examples/tasks.json", "w+") as f:
     f.write(card.to_json())
 
