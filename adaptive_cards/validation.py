057ddc2471df3c2e443f92ca0a80aaec08da2da3
Dennis Eder <dennis.eder91@web.de>
2024-11-30 16:32:36 +0100

    [feat] Extend validation and introduce corresponding tests

--- adaptive_cards/validation.py
+++ adaptive_cards/validation.py
@@ -1,59 +1,177 @@
 """Validation class for evaluating a cards schema"""
 
 import dataclasses
+import json
 from dataclasses import dataclass, fields
 from enum import Flag
-from typing import Any
+from typing import Any, Literal
+from abc import ABC, abstractmethod
+from pathlib import Path
+from enum import Enum
+from jsonschema.validators import Draft6Validator
+from jsonschema.exceptions import ValidationError
 from adaptive_cards.card import AdaptiveCard
 
 MINIMUM_VERSION_KEY: str = "min_version"
 
+SchemaVersion = Literal["1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6"]
+
 
 class Result(Flag):
     """
-    Represents a result value as a combination of flags.
-
-    Arguments:
-    SUCCESS = 0: Represents a successful result.
-    EMPTY_CARD = 1: Represents an empty card result.
-    INVALID_FIELD_VERSION = 2: Represents an invalid field version result.
-    UNDEFINED = 3: Represents an undefined result.
+    Represents the overall validation result value as a combination of flags.
     """
 
     SUCCESS = 0
-    EMPTY_CARD = 1
-    INVALID_FIELD_VERSION = 2
-    UNEDFINED = 3
+    """validation successful"""
+
+    FAILURE = 1
+    """validation failed"""
+
+
+class ValidationFailure(str, Enum):
+
+    EMPTY_CARD = "card body is empty"
+    """card doesn't contain any elements"""
+
+    INVALID_FIELD_VERSION = "field version exceeds card version"
+    """used elements might not yet be available for the defined card version"""
+
+    INVALID_SCHEMA = "official card schema violated"
+    """card structure does not meet the required schema"""
+
+    SIZE_LIMIT_EXCEEDED = "card exceeds the allowed card size for framework"
+    """size of card exceeds the defined limit for the target framework"""
+
+
+class AbstractTargetFramework(ABC):
+    """
+    Abstract interface representing the AbstractTargetFramework cards can be send to
+    """
+
+    def __init__(self, name: str, max_card_size_kb: float, schema_version: str) -> None:
+        self.__name: str = name
+        self.__max_card_size_kb: float = max_card_size_kb
+        self.__schema_version: str = schema_version
+
+    def name(self) -> str:
+        """
+        Return name of target framework
+
+        Returns:
+            str: Name of target framework
+        """
+        return self.__name
+
+    def max_card_size(self) -> float:
+        """
+        Calculate the maximum allowed card size for the target framework
+
+        Returns:
+            float: maximum card size
+        """
+        return self.__max_card_size_kb
+
+    def schema_version(self) -> str:
+        """
+        Schema version the card is validated against
+
+        Returns:
+            float: schema version
+        """
+        return self.__schema_version
+
+
+class MSTeamsFramework(AbstractTargetFramework):
+    """MSTeams target framework"""
+
+    def __init__(self):
+        """"""
+        name: str = "MS Teams"
+        max_card_size_kb: float = 28
+        schema_version: str = "1.5"
+        super().__init__(name, max_card_size_kb, schema_version)
+
+
+class BotFramework(AbstractTargetFramework):
+    """Bot target framework"""
+
+    def __init__(self):
+        """"""
+        name: str = "Bot"
+        max_card_size_kb: float = 28
+        schema_version: str = "1.5"
+        super().__init__(name, max_card_size_kb, schema_version)
+
+
+class CardValidatorAbstractFactory(ABC):
+    """Abstract card validator factory"""
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def create_validator_ms_teams(cls) -> "AbstractCardValidator":
+        """
+        Create card validator for target framework MS Teams
+
+        Returns:
+            AbstractCardValidator: Card validator for MS Teams as target framework
+        """
+
+    @classmethod
+    @abstractmethod
+    def create_validator_bot(cls) -> "AbstractCardValidator":
+        """
+        Create card validator for Bot framework
+
+        Returns:
+            AbstractCardValidator: Card validator for Bot framework
+        """
+
+
+class CardValidatorFactory(CardValidatorAbstractFactory):
+    """Factory for creating different kinds of card validators"""
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def create_validator_ms_teams(cls) -> "CardValidator":
+        return CardValidator(MSTeamsFramework(), "1.5")
+
+    @classmethod
+    def create_validator_bot(cls) -> "CardValidator":
+        return CardValidator(BotFramework(), "1.5")
 
 
 @dataclass
-class InvalidField:
+class Finding:
     """
-    Represents an invalid field within a parent type.
-
-    Attributes:
-        parent_type: The type of the parent object.
-        field_name: The name of the invalid field.
-        version: The version of the field.
+    Class for storing an individual finding during schema validation
     """
 
-    parent_type: str
-    field_name: str
-    version: str
+    failure: ValidationFailure
+    """Validation failure"""
+
+    message: str
+    """Corresponding failure message"""
+
+    message_additional: str = ""
+    """Addition information"""
 
 
-@dataclass
-class SchemaValidator:
+class AbstractCardValidator(ABC):
     """
-    Validator class for checking a cards schema w.r.t. to version numbers of individual fields.
+    Abstract interface for card validators
     """
 
-    def __init__(self) -> None:
-        self.__card: AdaptiveCard
-        self.__item: Any
-        self.__invalid_fields: list[InvalidField]
+    def __init__(self):
+        pass
 
-    def validate(self, card: AdaptiveCard) -> Result:
+    @abstractmethod
+    def validate(self, card: AdaptiveCard, debug: bool = True) -> Result:
         """
         Run validation on card.
 
@@ -63,28 +181,100 @@ class SchemaValidator:
         Returns:
             Result: Validation result
         """
+
+    @classmethod
+    @abstractmethod
+    def card_size(cls, card: AdaptiveCard) -> float:
+        """
+        Get size of card in KB
+
+        Args:
+            card (AdaptiveCard): Card the size should be calculated for
+
+        Returns:
+            float: Card size in KB
+        """
+
+    @abstractmethod
+    def details(self) -> list[Finding]:
+        """
+        Return a list of all individual findings occurred during the last validation. The data
+        will last until a new validation is executed.
+
+        Returns:
+            list[Finding]: List of findings
+        """
+
+
+class CardValidator(AbstractCardValidator):
+    """
+    Validator class for checking a cards schema w.r.t. to version numbers of individual fields,
+    card size and the overall card structure against the expected schema.
+    """
+
+    def __init__(
+        self,
+        target_framework: AbstractTargetFramework,
+        schema_version: SchemaVersion,
+    ) -> None:
+        self.__target_framework: AbstractTargetFramework = target_framework
+        self.__schema_version: SchemaVersion = schema_version
+
+        self.__card: AdaptiveCard
+        self.__item: Any
+        self.__findings: list[Finding]
+        self.__card_size: float
+
+    def validate(self, card: AdaptiveCard, debug: bool = True) -> Result:
         self.__card = card
         self.__reset()
-        result: Result = self.__validate_body()
-        self.__debug()
+        self.__validate_card()
 
-        return result
+        if debug:
+            self.__debug()
+
+        return Result.SUCCESS if len(self.__findings) == 0 else Result.FAILURE
+
+    @classmethod
+    def card_size(cls, card: AdaptiveCard) -> float:
+        return cls.__calculate_card_size(card)
+
+    def details(self) -> list[Finding]:
+        return self.__findings
 
     def __reset(self) -> None:
-        self.__invalid_fields: list[InvalidField] = []
+        self.__findings = []
+        self.__card_size: float = 0
 
-    def __validate_body(self) -> Result:
+    def __validate_card(self) -> None:
+        # check whether card body is empty or not
+        self.__validate_card_body()
+
+        # Check whether the overall card structure matches the expected schema
+        self.__validate_schema()
+
+        # check whether the version requirements are fulfilled for all elements
+        self.__validate_version_for_elements(self.__card.body)
+
+        # check whether the card size is within the expected range
+        # sizes are derived from the original documentation
+        # https://learn.microsoft.com/en-us/microsoftteams/platform/task-modules-and-cards/cards/cards-format?tabs=adaptive-md%2Cdesktop%2Cdesktop1%2Cdesktop2%2Cconnector-html#mention-support-within-adaptive-cards # pylint: disable=line-too-long
+        self.__validate_card_size()
+
+    def __validate_card_body(self) -> None:
         if self.__card.body is None:
-            return Result.EMPTY_CARD
+            self.__findings.append(
+                Finding(
+                    ValidationFailure.EMPTY_CARD,
+                    ValidationFailure.EMPTY_CARD.value,
+                )
+            )
 
-        self.__validate_elements(self.__card.body)
+    def __validate_version_for_elements(self, items: Any):
+        # return if no items at all
+        if not items:
+            return
 
-        if len(self.__invalid_fields) > 0:
-            return Result.INVALID_FIELD_VERSION
-
-        return Result.SUCCESS
-
-    def __validate_elements(self, items: Any):
         if not isinstance(items, list):
             items = [items]
 
@@ -111,24 +301,64 @@ class SchemaValidator:
                     )
 
         for iterable in iterables:
-            self.__validate_elements(iterable)
+            self.__validate_version_for_elements(iterable)
 
         for custom_type in custom_types:
-            self.__validate_elements(custom_type)
+            self.__validate_version_for_elements(custom_type)
+
+    @staticmethod
+    def __calculate_card_size(card: AdaptiveCard) -> float:
+        json_string: bytes = card.to_json().encode("utf-8")
+        return len(json_string) / 1024
+
+    def __validate_card_size(self) -> None:
+        self.__card_size = CardValidator.__calculate_card_size(self.__card)
+        if self.__card_size > self.__target_framework.max_card_size():
+            self.__findings.append(
+                Finding(
+                    ValidationFailure.SIZE_LIMIT_EXCEEDED,
+                    ValidationFailure.SIZE_LIMIT_EXCEEDED.value,
+                    f"{self.__target_framework.name()} | {self.__target_framework.max_card_size()} KB",
+                )
+            )
 
     def __validate_field_version(self, field_name: str, minimum_version: Any) -> None:
         assert minimum_version is not None
 
         if float(self.__card.version) < float(minimum_version):
-            self.__invalid_fields.append(
-                InvalidField(type(self.__item).__name__, field_name, minimum_version)
+            self.__findings.append(
+                Finding(
+                    ValidationFailure.INVALID_FIELD_VERSION,
+                    ValidationFailure.INVALID_FIELD_VERSION.value,
+                    f"Field version exceeds card version: {type(self.__item).__name__}"
+                    f" | {field_name} | {minimum_version}",
+                )
+            )
+
+    def __read_schema(self) -> dict[str, Any]:
+        with open(
+            Path(__file__)
+            .parent.joinpath("schemas")
+            .joinpath(f"schema-{self.__schema_version}.json"),
+            "+r",
+            encoding="utf-8",
+        ) as f:
+            return json.load(f)
+
+    def __validate_schema(self) -> None:
+        schema: dict[str, Any] = self.__read_schema()
+        try:
+            Draft6Validator(schema).validate(json.loads(self.__card.to_json()))
+
+        except ValidationError as ex:
+            self.__findings.append(
+                Finding(
+                    ValidationFailure.INVALID_SCHEMA,
+                    ValidationFailure.INVALID_SCHEMA.value,
+                    f"{ex.message}",
+                )
             )
 
     def __debug(self):
-        for invalid_field in self.__invalid_fields:
-            print(
-                f"Wrong version for field <{invalid_field.field_name}> "
-                f"in type <{invalid_field.parent_type}> | "
-                f"selected card version {self.__card.version} < minimum field version "
-                f"{invalid_field.version}"
-            )
+        for finding in self.__findings:
+            print(finding.message, finding.message_additional)
